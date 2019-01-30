14bca28b402bd3d48b89e761e6b0d4f26b74fe29
Deivison <deivisonoliveira.info@gmail.com>
2019-01-30 17:49:27 -0300

    Novo projeto #02
    entendendo funcionamento e utilizando razor

---
+++ UtilizandoRazor/Controllers/CondicoesController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UtilizandoRazor.Controllers
+{
+    public class CondicoesController : Controller
+    {
+        // GET: Condicoes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Condicoes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Condicoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Condicoes/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Condicoes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Condicoes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Condicoes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Condicoes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
