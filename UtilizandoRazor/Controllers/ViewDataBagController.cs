722a3ba63abb2288a35076a816c4e7722296a6d0
Deivison <deivisonoliveira.info@gmail.com>
2019-01-31 09:15:30 -0300

    Utilizando Razor
    Viewdata, viewBag, STV

---
+++ UtilizandoRazor/Controllers/ViewDataBagController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UtilizandoRazor.Models;
+
+namespace UtilizandoRazor.Controllers
+{
+    public class ViewDataBagController : Controller
+    {
+        // GET: ViewDataBag
+        public ActionResult Index()
+        {
+            ViewData["dataAtual"] = DateTime.Now.ToLongDateString();
+            ViewBag.horaAtual = DateTime.Now.ToLongTimeString();
+
+            return View();
+        }
+
+        /* forma de transporte de objetos com razor: viewdata e viewbag*/
+        public ActionResult exemplo2()
+        {
+            var conta = new Conta()
+            {
+                idConta = 1,
+                numeroConta = 001659,
+                nomeCliente = "joão da silva",
+                saldoAtual = 1535.0,
+                limiteChequeEspecial = 500.0,
+                dataEntrada = DateTime.Now.Date
+            };
+
+            ViewData["conta"] = conta;
+            ViewBag.conta = conta;
+
+            return View();
+        }
+
+        /* forma 2 de transporte de objetos com razor: strongly typed views stv*/
+        public ActionResult exemplo3()
+        {
+            var conta = new Conta
+            {
+                idConta = 2,
+                numeroConta = 0332,
+                nomeCliente = "Maria Silva",
+                saldoAtual = 2400.0,
+                limiteChequeEspecial = 1400.0,
+                dataEntrada = DateTime.Now.Date
+            };
+
+            return View(conta);
+        }
+
+
+        // GET: ViewDataBag/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ViewDataBag/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ViewDataBag/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ViewDataBag/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ViewDataBag/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ViewDataBag/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ViewDataBag/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ UtilizandoRazor/Models/Conta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UtilizandoRazor.Models
+{
+    public class Conta
+    {
+        public int idConta { get; set; }
+        public int numeroConta { get; set; }
+        public string tipoDeConta { get; set; }
+        public string nomeCliente { get; set; }
+        public double saldoAtual { get; set; }
+        public double limiteChequeEspecial { get; set; }
+        public DateTime dataEntrada { get; set; }
+        public bool indicaAtivo { get; set; }
+    }
+}
